5fe317651bf9f890cfd36afab9fd1cbe9594b683
Sriram Govindan <sriramgovindanwork@gmail.com>
2021-07-24 00:50:29 -0700

    shape changes for some tensors in linear regression

--- Regression/Linear.py
+++ Regression/Linear.py
@@ -1,33 +1,28 @@
 import torch
-from CostFunctions.LossFunctions import squared_error
-from Optimizers.GradientDescent import batch_grad
+
+
+def make_feature_list(x_feature_list):
+    return torch.cat((torch.ones(len(x_feature_list), 1), torch.FloatTensor(x_feature_list)), 1)
 
 
 class LinearRegression:
 
     def __init__(self,
                  feature_list,
-                 target_list,
-                 learning_rate):
-        self.__feature_list = torch.cat((torch.ones(len(feature_list), 1), torch.FloatTensor(feature_list)), 1)
+                 target_list):
+        self.__feature_list = make_feature_list(feature_list)
         self.__target_list = torch.FloatTensor(target_list)
         self.__parameters = torch.rand(1, self.__feature_list.size()[1])
-        self.__learning_rate = learning_rate
 
-        self.__error_list = []
-
-    def __get_predictions(self):
+    def get_predictions(self):
         prediction = torch.matmul(self.__feature_list, torch.transpose(self.__parameters, 0, 1))
-        return prediction
+        return torch.squeeze(torch.transpose(prediction, 0, 1), dim=0)
 
     def normal_eqt(self):
         mat_1 = torch.matmul(torch.transpose(self.__feature_list, 0, 1), self.__feature_list)
-
         target = torch.unsqueeze(self.__target_list, dim=0)
-
         mat_2 = torch.matmul(torch.transpose(self.__feature_list, 0, 1), torch.transpose(target, 0, 1))
-
-        return torch.matmul(torch.inverse(mat_1), mat_2)
+        return torch.unsqueeze(torch.squeeze(torch.matmul(torch.inverse(mat_1), mat_2), dim=1), dim=0)
 
     def get_parameters(self):
         return self.__parameters
@@ -38,19 +33,8 @@ class LinearRegression:
     def get_feature_list(self):
         return self.__feature_list
 
-    def get_learning_rate(self):
-        return
+    def update_parameters(self, theta_param_list):
+        self.__parameters = theta_param_list
 
-    def train_model(self):
-        predictions = torch.squeeze(torch.transpose(self.__get_predictions(), 0, 1), dim=0)
-
-        error = squared_error(predictions, self.__target_list)
-        self.__error_list.append(error)
-
-        self.__parameters = torch.unsqueeze(batch_grad(self.__learning_rate,
-                                                       self.__feature_list,
-                                                       self.__target_list,
-                                                       predictions,
-                                                       torch.squeeze(self.__parameters, dim=0)), dim=0)
-
-        return error, predictions
+    def set_target_list(self, y_target_list):
+        self.__target_list = y_target_list
