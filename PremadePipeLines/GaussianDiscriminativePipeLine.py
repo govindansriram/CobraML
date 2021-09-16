fixed covariance matrix

PremadePipeLines/GaussianDiscriminativePipeLine.py
@@ -1,32 +1,39 @@
 import torch
 from torch.utils.data import DataLoader
+from GeneralMethods.GeneralDataset import GenericDataSet
 from GeneralMethods.StatsMethods import multivariate_normal_distribution
 
 
 class BinaryGDA:
 
     def __init__(self,
-                 feature_count: int,
-                 train_data_loader: DataLoader,
-                 test_data_loader: DataLoader,
+                 feature_data: list[list[float]],
+                 target_data: list[int],
+                 batch_size: int,
                  device="cpu"):
-        self.__train_loader = train_data_loader
-        self.__test_loader = test_data_loader
 
-        self.__feature_count = feature_count
+        self.__feature_count = len(feature_data[0])
+
+        data_set = GenericDataSet(torch.tensor(feature_data, dtype=torch.float),
+                                  torch.tensor(target_data, dtype=torch.float))
+
+        self.__data_loader = DataLoader(data_set,
+                                        batch_size=batch_size,
+                                        shuffle=True)
 
         self.__device = torch.device(device=device)
-        self.__mu_one_vector = torch.zeros(size=(feature_count, 1),
+        self.__mu_one_vector = torch.zeros(size=(self.__feature_count, 1),
                                            dtype=torch.float64,
                                            device=self.__device)
 
-        self.__mu_zero_vector = torch.zeros(size=(feature_count, 1),
+        self.__mu_zero_vector = torch.zeros(size=(self.__feature_count, 1),
                                             dtype=torch.float64,
                                             device=self.__device)
 
-        self.__co_variance_matrix = torch.zeros(size=(feature_count, feature_count),
+        self.__co_variance_matrix = torch.zeros(size=(self.__feature_count, self.__feature_count),
                                                 dtype=torch.float64,
                                                 device=self.__device)
+
         self.__fi = 0
         self.__total_count = 0
 
@@ -35,7 +42,7 @@ class BinaryGDA:
         one_count = 0
         zero_count = 0
 
-        for batch in self.__train_loader:
+        for batch in self.__data_loader:
             feature_tensor, target_tensor = batch
 
             feature_tensor = feature_tensor.to(self.__device)
@@ -43,13 +50,13 @@ class BinaryGDA:
 
             full_tensor = torch.cat((feature_tensor, target_tensor), dim=1)
 
-            mask = (full_tensor[:, self.__feature_count + 1] == 0)
+            mask = (full_tensor[:, self.__feature_count] == 1)
 
-            zero_matrix = full_tensor[mask][:, :self.__feature_count]
-            one_matrix = full_tensor[~mask][:, :self.__feature_count]
+            zero_matrix = full_tensor[~mask][:, :self.__feature_count]
+            one_matrix = full_tensor[mask][:, :self.__feature_count]
 
-            zero_count += zero_matrix.shape()[0]
-            one_count += one_matrix.shape()[0]
+            zero_count += zero_matrix.size()[0]
+            one_count += one_matrix.size()[0]
 
             zero_sum = torch.unsqueeze(torch.sum(zero_matrix, dim=0), dim=0).T
             one_sum = torch.unsqueeze(torch.sum(one_matrix, dim=0), dim=0).T
@@ -59,8 +66,8 @@ class BinaryGDA:
 
         self.__fi = one_count / (one_count + zero_count)
 
-        self.__mu_one_vector / one_count
-        self.__mu_zero_vector / zero_count
+        self.__mu_one_vector /= one_count
+        self.__mu_zero_vector /= zero_count
 
         self.__total_count = one_count + zero_count
 
@@ -68,21 +75,21 @@ class BinaryGDA:
 
     def __make_covariance_matrix(self):
 
-        for batch in self.__train_loader:
+        for batch in self.__data_loader:
 
-            for feature_vector, target_tensor in batch:
+            joint_tensor = torch.cat((batch[0].to(self.__device),
+                                      batch[1].to(self.__device)),
+                                     dim=1)
 
-                if target_tensor[0] == 0:
-                    sigma = feature_vector - self.__mu_zero_vector
+            for joint_vector in joint_tensor:
+
+                joint_vector = torch.unsqueeze(joint_vector, dim=0)
+
+                if joint_vector[0][joint_vector.size()[1] - 1] == 0:
+                    sigma = (joint_vector[:, :joint_vector.size()[1] - 1] - self.__mu_zero_vector.T).T
                 else:
-                    sigma = feature_vector - self.__mu_one_vector
+                    sigma = (joint_vector[:, :joint_vector.size()[1] - 1] - self.__mu_one_vector.T).T
 
                 self.__co_variance_matrix += torch.mm(sigma, sigma.T)
 
         self.__co_variance_matrix /= self.__total_count
-
-
-if __name__ == '__main__':
-    test_ten = torch.tensor([1, 1, 2])
-
-    print(1 / test_ten)